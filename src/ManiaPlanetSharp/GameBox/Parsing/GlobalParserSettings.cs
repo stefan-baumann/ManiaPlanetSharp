e2ad4999565b407b1710cb15cd043ee1c80a7051
Stefan Baumann <stefan-baumann@outlook.com>
2020-03-16 16:02:39 +0100

    ParsingErrorLogger and global parser settings

---
+++ src/ManiaPlanetSharp/GameBox/Parsing/GlobalParserSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManiaPlanetSharp.GameBox.Parsing
+{
+    public static class GlobalParserSettings
+    {
+        public static uint MaximumUncompressedGameBoxBodySize { get; } = 100000000; //100MB
+    }
+}
---
+++ src/ManiaPlanetSharp/GameBox/Parsing/ParsingErrorLogger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManiaPlanetSharp.GameBox.Parsing
+{
+    public static class ParsingErrorLogger
+    {
+        public static event EventHandler<ParsingErrorEventArgs> ParsingErrorOccured;
+
+        internal static void OnParsingErrorOccured(object sender, ParsingErrorEventArgs args)
+        {
+            ParsingErrorOccured?.Invoke(sender, args);
+        }
+    }
+
+    public class ParsingErrorEventArgs
+        : EventArgs
+    {
+        public ParsingErrorEventArgs(uint chunkId, string message)
+        {
+            this.ChunkId = chunkId;
+            this.Message = message;
+        }
+
+        public uint ChunkId { get; private set; }
+        public string Message { get; private set; }
+
+        public override string ToString() => $"Chunk 0x{this.ChunkId:X8}.\n\n{this.Message}";
+    }
+
+    public class BodyParsingErrorEventArgs
+        : ParsingErrorEventArgs
+    {
+        public BodyParsingErrorEventArgs(uint chunkId, long position, string message)
+            : base(chunkId, message)
+        {
+            this.Position = position;
+        }
+
+        public long Position { get; private set; }
+
+        public override string ToString() => $"Chunk 0x{this.ChunkId:X8} at {this.Position}.\n\n{this.Message}";
+    }
+
+    public class NodeInternalParsingErrorEventArgs
+        : ParsingErrorEventArgs
+    {
+        public NodeInternalParsingErrorEventArgs(Node node, uint chunkId, string message)
+            : base(chunkId, message)
+        {
+            this.ClassId = node.ClassId;
+        }
+
+        public uint ClassId { get; private set; }
+
+        public override string ToString() => $"Chunk 0x{this.ChunkId:X8} in Node 0x{this.ClassId:X8}.\n\n{this.Message}";
+    }
+}
