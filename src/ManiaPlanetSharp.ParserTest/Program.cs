ac4413b0cc2e6662ffe34fab617aa4341cf0c066
Stefan Baumann <stefan-baumann@outlook.com>
2020-03-17 00:06:14 +0100

    Metadata providers

--- src/ManiaPlanetSharp.ParserTest/Program.cs
+++ src/ManiaPlanetSharp.ParserTest/Program.cs
@@ -1,20 +1,22 @@
 ﻿using ManiaPlanetSharp.GameBox;
+using ManiaPlanetSharp.GameBox.MetadataProviders;
 using ManiaPlanetSharp.GameBox.Parsing;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace ManiaPlanetSharp.ParserTest
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
 
             while (true)
             {
-                Console.Write("Please enter a path to a GameBox file: ");
+                Console.Write("GameBox file: ");
                 string path = Console.ReadLine();
                 if (path.StartsWith("\"") && path.EndsWith("\""))
                 {
@@ -23,12 +25,18 @@ namespace ManiaPlanetSharp.ParserTest
                 if (File.Exists(path))
                 {
                     using (MemoryStream stream = new MemoryStream(File.ReadAllBytes(path)))
-                    using (GameBoxReader reader = new GameBoxReader(stream))
                     {
-                        CustomStructParser<GameBoxFile> parser = ParserFactory.GetCustomStructParser<GameBoxFile>();
                         Stopwatch stopwatch = Stopwatch.StartNew();
-                        GameBoxFile file = parser.Parse(reader);
+                        GameBoxFile file = GameBoxFile.Parse(stream);
                         stopwatch.Stop();
+
+                        MetadataProvider metadataProvider = new ItemMetadataProvider(file);
+                        Console.WriteLine("Metadata:");
+                        foreach(var property in metadataProvider.GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).Where(property => property.Name != "File"))
+                        {
+                            Console.WriteLine($" - {property.Name}: {property.GetValue(metadataProvider)}");
+                        }
+
                         Console.WriteLine($"Done in {stopwatch.Elapsed.TotalMilliseconds:#0.0}ms.");
                     }
                 }
