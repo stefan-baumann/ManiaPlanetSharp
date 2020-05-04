97212c778c852f3295568283858b2758a9709f00
Stefan Baumann <stefan-baumann@outlook.com>
2020-05-04 17:19:22 +0200

    Make GameBoxReader return id on unknown lookback string

--- src/ManiaPlanetSharp/GameBox/Parsing/GameBoxReader.cs
+++ src/ManiaPlanetSharp/GameBox/Parsing/GameBoxReader.cs
@@ -306,7 +306,7 @@ namespace ManiaPlanetSharp.GameBox.Parsing
             if (storedIndex > this.LookbackStrings.Count)
             {
                 Debug.WriteLine($"Lookback String with Index {storedIndex} (0x{storedIndex:X8}) could not be found.");
-                return null;
+                return $"[Unknown lookback string #{storedIndex}]";
             }
             else
             {
