8aeb6fd35fe885460b093df4be95ae45f2e52f07
Adam Bieńkowski <donadigos159@gmail.com>
2020-07-04 21:45:07 +0200

    Add parsing checkpoint times for replays

---
+++ src/ManiaPlanetSharp/GameBox/Parsing/Chunks/Ghost/GhostCheckpointEntriesChunk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManiaPlanetSharp.GameBox.Parsing.Chunks
+{
+    [CustomStruct]
+    public class CheckpointEntry
+    {
+        [Property]
+        public uint Time { get; set; }
+
+        [Property]
+        public uint Flags { get; set; }
+    }
+
+    [Chunk(0x0309200B, Skippable = true)]
+    public class GhostCheckpointEntriesChunk
+        : Chunk
+    {
+        [Property]
+        public uint CheckpointEntryCount { get; set; }
+
+        [Property, Array(nameof(CheckpointEntryCount))]
+        public CheckpointEntry[] CheckpointEntries { get; set; }
+    }
+}
