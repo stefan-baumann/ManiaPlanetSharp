stefan-baumann/ManiaPlanetSharp

commit 27a9f4e2181388e76a7070021867d20bd3f4667c

Fix stream position after parsing an unskippable chunk

src/ManiaPlanetSharp/GameBox/GameBoxFile.cs
@@ -338,6 +338,7 @@ namespace ManiaPlanetSharp.GameBox
                             else
                             {
                                 chunks.Add(parser.Parse(reader, id));
+                                offset = stream.Position;
                             }
 
                         }

commit d51c659d9ec6c25173da6e9821485aea7b31cbec

Fix parser bug introduced in 27a9f4e2 (#24)

src/ManiaPlanetSharp/GameBox/GameBoxFile.cs
@@ -338,7 +338,7 @@ namespace ManiaPlanetSharp.GameBox
                             else
                             {
                                 chunks.Add(parser.Parse(reader, id));
-                                offset = stream.Position;
+                                offset = stream.Position - 1;
                             }
 
                         }