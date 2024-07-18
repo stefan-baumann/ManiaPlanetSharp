46ab678b5140883557ff0856b53f809d9f5666dd
tomek0055 <kemot00055@gmail.com>
2024-07-18 13:31:00 +0200

    Disable 0x0301B000 chunk

--- src/ManiaPlanetSharp/GameBox/Parsing/Chunks/Collector/CollectorListChunk.cs
+++ src/ManiaPlanetSharp/GameBox/Parsing/Chunks/Collector/CollectorListChunk.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ManiaPlanetSharp.GameBox.Parsing.Chunks
 {
-    [Chunk(0x0301B000)]
+    //[Chunk(0x0301B000)]
     public class CollectorListChunk
         : Chunk
     {        
